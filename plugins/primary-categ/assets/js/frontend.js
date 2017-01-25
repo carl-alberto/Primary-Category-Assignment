11161c12a5069d27e4d1128a7177c58e76dc7867
carl alberto <cralberto11@gmail.com>
2017-01-25 17:13:58 +0800

    updated a search issue

--- plugins/primary-categ/assets/js/frontend.js
+++ plugins/primary-categ/assets/js/frontend.js
@@ -13,7 +13,7 @@ jQuery( document ).ready(function( $ ) {
 	});
 
 	function SearchString() {
-		return '/?q=' + $( '.search-field' ).val();
+		return '/?s=' + $( '.search-field' ).val();
 	}
 
 	function SearchTaxonomy() {
